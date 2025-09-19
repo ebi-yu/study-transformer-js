7b09ac0cf01f54b85b0a0ddd34359bbf7009cfa8
ebi-yu <ebiDevelopper1088@gmail.com>
2025-09-19 09:05:49 +0900

    WIP : 実装中

---
+++ samples/vue/src/App.vue
@@ -0,0 +1,128 @@
+<script setup>
+import { onMounted, onUnmounted, ref } from "vue";
+import LanguageSelector from "./components/LanguageSelector.vue";
+import Progress from "./components/Progress.vue";
+
+// 状態管理
+const ready = ref(null);
+const disabled = ref(false);
+const progressItems = ref([]);
+
+const input = ref("I love walking my dog.");
+const sourceLanguage = ref("eng_Latn");
+const targetLanguage = ref("fra_Latn");
+const output = ref("");
+
+// Worker 参照
+let worker = null;
+
+onMounted(() => {
+  if (!worker) {
+    worker = new Worker(new URL("./worker.js", import.meta.url), {
+      type: "module",
+    });
+  }
+
+  const onMessageReceived = (e) => {
+    switch (e.data.status) {
+      case "initiate":
+        ready.value = false;
+        progressItems.value.push(e.data);
+        break;
+
+      case "progress":
+        progressItems.value = progressItems.value.map((item) =>
+          item.file === e.data.file
+            ? { ...item, progress: e.data.progress }
+            : item
+        );
+        break;
+
+      case "done":
+        progressItems.value = progressItems.value.filter(
+          (item) => item.file !== e.data.file
+        );
+        break;
+
+      case "ready":
+        ready.value = true;
+        break;
+
+      case "update":
+        output.value = e.data.output;
+        break;
+
+      case "complete":
+        disabled.value = false;
+        break;
+    }
+  };
+
+  worker.addEventListener("message", onMessageReceived);
+
+  onUnmounted(() => {
+    worker.removeEventListener("message", onMessageReceived);
+  });
+});
+
+const translate = () => {
+  disabled.value = true;
+  worker.postMessage({
+    text: input.value,
+    src_lang: sourceLanguage.value,
+    tgt_lang: targetLanguage.value,
+  });
+};
+</script>
+
+<template>
+  <h1>Transformers.js</h1>
+  <h2>ML-powered multilingual translation in Vue 3!</h2>
+
+  <div class="container">
+    <div class="language-container">
+      <LanguageSelector
+        type="Source"
+        :defaultLanguage="'eng_Latn'"
+        @change="(x) => (sourceLanguage.value = x.target.value)"
+      />
+      <LanguageSelector
+        type="Target"
+        :defaultLanguage="'fra_Latn'"
+        @change="(x) => (targetLanguage.value = x.target.value)"
+      />
+    </div>
+
+    <div class="textbox-container">
+      <textarea v-model="input" rows="3"></textarea>
+      <textarea v-model="output" rows="3" readonly></textarea>
+    </div>
+  </div>
+
+  <button :disabled="disabled" @click="translate">Translate</button>
+
+  <div class="progress-bars-container">
+    <label v-if="ready === false">Loading models... (only run once)</label>
+    <div v-for="data in progressItems" :key="data.file">
+      <Progress :text="data.file" :percentage="data.progress" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin: 1rem 0;
+}
+.language-container {
+  display: flex;
+  gap: 1rem;
+}
+.textbox-container {
+  display: flex;
+  gap: 1rem;
+  margin-top: 1rem;
+}
+.progress-bars-container {
+  margin-top: 1rem;
+}
+</style>
